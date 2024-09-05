d1ccc0f90855edcdc23c39444c507fb5e4da128d
vaibhav4434 <vsonawane4434@gmail.com>
2024-09-05 13:19:35 +0530

    Update streamlit_app.py
    adding changes into the columns

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,8 +19,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #ST.stop()
 #convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC fucntion
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 ingredients_list = st.multiselect('choose up to 5 ingredients:',my_dataframe,max_selections = 5)
 
 if ingredients_list:
@@ -28,6 +28,8 @@ if ingredients_list:
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         st.subheader(fruit_chosen + ' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" + fruit_chosen)
 #st.text(fruityvice_response.json())
